82aa306ce476c53d30db4e3f89d5827db2d80ec3
mvujas <18309829+mvujas@users.noreply.github.com>
2021-12-15 21:33:19 +0100

    scroll to top added

---
+++ app/js/base.js
@@ -0,0 +1,5 @@
+$(document).ready(() => {
+    $("#scroll-top-button").on("click", () => {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    });
+});
---
+++ docs/js/base.js
@@ -0,0 +1,2 @@
+"use strict";$(document).ready(function(){$("#scroll-top-button").on("click",function(){window.scrollTo({top:0,behavior:"smooth"})})});
+//# sourceMappingURL=base.js.map
